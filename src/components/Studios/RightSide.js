5d8cd0c371d9ca385eb889937834c6b47bab5fd4
Kristen <kriscadacio@gmail.com>
2023-04-04 21:40:20 -0400

    Fixed description and added new data to format the studio spects

--- src/components/Studios/RightSide.js
+++ src/components/Studios/RightSide.js
@@ -3,12 +3,16 @@ import IndividualStudios from './IndividualStudios'
 
 function RightSide({studioDetails}) {
 
-    const {title} = studioDetails
+    const {title, description, sqftg, height} = studioDetails
 
   return (
     <>
         <div className="rightSideContainer">
             <h1 className="studioTitle">{title}</h1>
+            <p>{description}</p>
+            <br/>
+            <p>Square Footage: {sqftg}</p>
+            <p>Height: {height}</p>
         </div>
     </>
 
