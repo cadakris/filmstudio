a6edb241eabb609ba4926cccda5f615dd9c33267
Kristen <kriscadacio@gmail.com>
2023-04-25 22:55:45 -0400

    Added footer to contact section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,16 +1,17 @@
 import React from 'react'
 
-
+import Navbar from '../Navbar/Navbar'
 import ContactParallax from './ContactParallax'
+import MainFooter from '../Footer/MainFooter'
 
 import './_contact.scss'
 
 function Contact() {
   return (
     <>
-        <div>
-           <ContactParallax />
-        </div>
+        <Navbar />
+        <ContactParallax />
+        <MainFooter />
     </>
   )
 }
