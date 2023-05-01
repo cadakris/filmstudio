d793cbd363846c40c406964903442153af7f5d3f
Kristen <kriscadacio@gmail.com>
2023-05-01 12:05:26 -0400

    Added content to contact page

--- src/components/Contact/LeftMessage.js
+++ src/components/Contact/LeftMessage.js
@@ -2,7 +2,32 @@ import React from 'react'
 
 function LeftMessage() {
   return (
-    <div>LeftMessage</div>
+    <>
+        <div className='left-side-split'>
+            <h1></h1>
+            <form>
+                <div className="form-container">
+                    <input className="half"
+                    type="text"
+                    placeholder='First Name'
+                    />
+                    <input className="half"
+                    type="text"
+                    placeholder='Last Name'
+                    />
+                    <input className="full"
+                    type="text"
+                    placeholder='Email'
+                    />
+                    <textarea className="message"
+                    type='text'
+                    name='message'
+                    placeholder='Write Your Message Here'
+                    />
+                </div>
+            </form>
+        </div>
+    </>
   )
 }
 
